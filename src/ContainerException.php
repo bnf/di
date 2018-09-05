69a3f087833071c7aa634a562b27a2fecee1bb80
Benjamin Franzke <benjaminfranzke@gmail.com>
2018-09-05 11:06:25 +0200

    Reconcile Exception class coding style

--- src/ContainerException.php
+++ src/ContainerException.php
@@ -2,8 +2,9 @@
 declare(strict_types = 1);
 namespace Bnf\Di;
 
+use Exception;
 use Psr\Container\ContainerExceptionInterface;
 
-class ContainerException extends \Exception implements ContainerExceptionInterface
+class ContainerException extends Exception implements ContainerExceptionInterface
 {
 }
